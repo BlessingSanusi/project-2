4a85f48c3a08870524567ffe27a6de3ed71c29cc
Matthew Yokopovich <matthew.yokopovich@gmail.com>
2019-10-03 20:35:32 -0500

    changes to apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,23 +2,57 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  app.get("/api/user", function(req, res) {
+    db.User.findAll({}).then(function(dbUsers) {
+      res.json(dbUsers);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  app.post("/api/user", function(req, res) {
+    db.User.create(req.body).then(function(dbUser) {
+      res.json(dbUser);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+  app.delete("/api/user/:id", function(req, res) {
+    db.User.destroy({ where: { id: req.params.id } }).then(function(dbUser) {
+      res.json(dbUser);
+    });
+  });
+  app.get("/api/post", function(req, res) {
+    db.Post.findAll({}).then(function(dbPosts) {
+      res.json(dbPosts);
+    });
+  });
+
+  // Create a new example
+  app.post("/api/post", function(req, res) {
+    db.Post.create(req.body).then(function(dbPost) {
+      res.json(dbPost);
+    });
+  });
+
+  // Delete an example by id
+  app.delete("/api/post/:id", function(req, res) {
+    db.Post.destroy({ where: { id: req.params.id } }).then(function(dbPost) {
+      res.json(dbPost);
+    });
+  });
+
+
+  // Create a new example
+  app.post("/api/comment", function(req, res) {
+    db.Comment.create(req.body).then(function(dbComment) {
+      res.json(dbComment);
+    });
+  });
+
+  // Delete an example by id
+  app.delete("/api/comment/:id", function(req, res) {
+    db.Comment.destroy({ where: { id: req.params.id } }).then(function(dbComment) {
+      res.json(dbComment);
     });
   });
 };
