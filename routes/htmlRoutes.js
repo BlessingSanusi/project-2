3a2ebca44489877fed4aef94409fb0692d27d7cc
Joshua R. Munoz <jrmunoz@Joshuas-MacBook-Pro.local>
2019-10-05 11:53:25 -0500

    clean up node

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,30 @@
+/*-----------------------\
+ PATHS FOR DIFF HTML PAGES
+\-----------------------*/
+const path = require("path");
+var db = require("../models");
+
+module.exports = function(app) {
+  // Load index page
+  app.get("/", function(req, res) {
+    res.render("../views/index.handlebars");
+  });
+  
+  app.get("/about", function(req, res) {
+    res.render("../views/about.handlebars");
+  });
+
+  // Load example page and pass in an example by id
+  // app.get("/example/:id", function(req, res) {
+  //   db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  //     res.render("example", {
+  //       example: dbExample
+  //     });
+  //   });
+  // });
+
+  // Render 404 page for any unmatched routes
+  app.get("*", function(req, res) {
+    res.render("404");
+  });
+};
