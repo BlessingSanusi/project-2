e8bed69d565aae4f3e03936d474627d1efa6d2d9
Matthew Yokopovich <matthew.yokopovich@gmail.com>
2019-10-05 14:00:05 -0500

    final changes to model(hopefully)

--- models/seed.sql
+++ models/seed.sql
@@ -1,3 +1,3 @@
 USE recipedb;
-INSERT INTO users (name, password, joinDate)
+INSERT INTO users (username, password, joinDate)
 VALUES ('Arin Hanson', 'chinnigan', '2016-12-31'), ('Dan Avidan', 'notsogrump', '2016-01-01'), ('Matt Watson', 'ree', '2015-01-01'), ('Ryan McGee', 'mcgoggles', '2015-12-01');
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,54 +1,55 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
+  // Get all users
   app.get("/api/user", function(req, res) {
     db.User.findAll({}).then(function(dbUsers) {
       res.json(dbUsers);
     });
   });
 
-  // Create a new example
+  // Create a new user
   app.post("/api/user", function(req, res) {
     db.User.create(req.body).then(function(dbUser) {
       res.json(dbUser);
     });
   });
 
-  // Delete an example by id
+  // Delete an user by id
   app.delete("/api/user/:id", function(req, res) {
     db.User.destroy({ where: { id: req.params.id } }).then(function(dbUser) {
       res.json(dbUser);
     });
   });
+  //get all posts
   app.get("/api/post", function(req, res) {
     db.Post.findAll({}).then(function(dbPosts) {
       res.json(dbPosts);
     });
   });
 
-  // Create a new example
+  // Create a new post
   app.post("/api/post", function(req, res) {
     db.Post.create(req.body).then(function(dbPost) {
       res.json(dbPost);
     });
   });
 
-  // Delete an example by id
+  // Delete an post by id
   app.delete("/api/post/:id", function(req, res) {
     db.Post.destroy({ where: { id: req.params.id } }).then(function(dbPost) {
       res.json(dbPost);
     });
   });
 
-  // Create a new example
+  // Create a new comment
   app.post("/api/comment", function(req, res) {
     db.Comment.create(req.body).then(function(dbComment) {
       res.json(dbComment);
     });
   });
 
-  // Delete an example by id
+  // Delete an comment by id
   app.delete("/api/comment/:id", function(req, res) {
     db.Comment.destroy({ where: { id: req.params.id } }).then(function(
       dbComment
