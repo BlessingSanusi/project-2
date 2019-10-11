946c1614dec976480e64650c7f742bcb346bc58d
Joshua R. Munoz <jrmunoz@Joshuas-MacBook-Pro.local>
2019-10-10 19:43:11 -0500

    more ui stuffs

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,14 +4,25 @@
 const path = require("path");
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
+  app.get("/", function (req, res) {
     res.render("../views/index.handlebars");
   });
-  
-  app.get("/about", function(req, res) {
-    res.render("../views/about.handlebars");
+
+  // Load Page that shows Category 1
+  app.get("/category1", function (req, res) {
+    res.render("../views/category1.handlebars");
+  });
+
+  // Load Page that shows Category 2
+  app.get("/category2", function (req, res) {
+    res.render("../views/category2.handlebars");
+  });
+
+  // Load Page that shows Category 3
+  app.get("/category3", function (req, res) {
+    res.render("../views/category3.handlebars");
   });
 
   // Load example page and pass in an example by id
@@ -24,7 +35,7 @@ module.exports = function(app) {
   // });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
